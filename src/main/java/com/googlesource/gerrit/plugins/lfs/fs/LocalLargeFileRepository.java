c01abbceb6afbd7e61a77dded0243f873cecd1bc
jcentkowski <root@ctf-centos72-dev-box.collab.net>
2016-07-20 11:37:31 +0200

    Fix for empty plugin configuration page being shown
    Issue:
    According to [1] when plugin handles "/" path it has to serve
    documentation pages too. LfsFsContentServlet is registered for "/*" path
    
    Solution:
    Register LfsContentServlet for "/content/*" path and make sure that
    repository serves files for it too.
    
    [1] 
    https://gerrit-review.googlesource.com/Documentation/dev-plugins.html#auto-index
    
    Change-Id: I01f27b8706ca388dc029963c2074a1fb66d9a6f8

--- src/main/java/com/googlesource/gerrit/plugins/lfs/fs/LocalLargeFileRepository.java
+++ src/main/java/com/googlesource/gerrit/plugins/lfs/fs/LocalLargeFileRepository.java
@@ -30,12 +30,14 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class LocalLargeFileRepository extends FileLfsRepository {
+  public static final String CONTENT_PATH = "content/";
+
   @Inject
   LocalLargeFileRepository(PluginConfigFactory cfg,
       @PluginName String pluginName,
       @PluginCanonicalWebUrl String url,
       @PluginData Path defaultDataDir) throws IOException {
-    super(url, getOrCreateDataDir(cfg, pluginName, defaultDataDir));
+    super(url + CONTENT_PATH, getOrCreateDataDir(cfg, pluginName, defaultDataDir));
   }
 
   private static Path getOrCreateDataDir(PluginConfigFactory cfgFactory,
