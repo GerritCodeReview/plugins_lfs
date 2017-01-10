76fc25598b01c3ba09857e992d86c3c3801298db
Jacek Centkowski <geminica.programs@gmail.com>
2017-01-10 23:09:31 +0100

    Introduce HTTP Basic Auth to LfsApiServlet
    According to [1] Git LFS client uses HTTP Basic Auth to authorize
    requests. This patch verifies if user has permission to perform
    requested operation.
    
    [1]
    https://github.com/git-lfs/git-lfs/blob/master/docs/api/authentication.md
    Change-Id: I28864fdaaf701e06fa9f60e7e913bc4a15da7b1d
    Signed-off-by: Jacek Centkowski <geminica.programs@gmail.com>

--- src/main/java/com/googlesource/gerrit/plugins/lfs/LfsApiServlet.java
+++ src/main/java/com/googlesource/gerrit/plugins/lfs/LfsApiServlet.java
@@ -19,8 +19,11 @@ import static com.google.gerrit.extensions.client.ProjectState.READ_ONLY;
 import static com.google.gerrit.httpd.plugins.LfsPluginServlet.URL_REGEX;
 
 import com.google.gerrit.common.ProjectUtil;
+import com.google.gerrit.common.data.Capable;
 import com.google.gerrit.reviewdb.client.Project;
+import com.google.gerrit.server.CurrentUser;
 import com.google.gerrit.server.project.ProjectCache;
+import com.google.gerrit.server.project.ProjectControl;
 import com.google.gerrit.server.project.ProjectState;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -28,36 +31,42 @@ import com.google.inject.Singleton;
 import org.eclipse.jgit.lfs.errors.LfsException;
 import org.eclipse.jgit.lfs.errors.LfsRepositoryNotFound;
 import org.eclipse.jgit.lfs.errors.LfsRepositoryReadOnly;
+import org.eclipse.jgit.lfs.errors.LfsUnauthorized;
 import org.eclipse.jgit.lfs.errors.LfsUnavailable;
 import org.eclipse.jgit.lfs.errors.LfsValidationError;
 import org.eclipse.jgit.lfs.server.LargeFileRepository;
 import org.eclipse.jgit.lfs.server.LfsObject;
-import org.eclipse.jgit.lfs.server.LfsProtocolServlet;
+import org.eclipse.jgit.lfs.server.LfsGerritProtocolServlet;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Singleton
-public class LfsApiServlet extends LfsProtocolServlet {
+public class LfsApiServlet extends LfsGerritProtocolServlet {
   private static final long serialVersionUID = 1L;
   private static final Pattern URL_PATTERN = Pattern.compile(URL_REGEX);
+  private static final String UPLOAD = "upload";
 
   private final ProjectCache projectCache;
   private final LfsConfigurationFactory lfsConfigFactory;
   private final LfsRepositoryResolver repoResolver;
+  private final LfsAuthUserProvider userProvider;
 
   @Inject
   LfsApiServlet(ProjectCache projectCache,
       LfsConfigurationFactory lfsConfigFactory,
-      LfsRepositoryResolver repoResolver) {
+      LfsRepositoryResolver repoResolver,
+      LfsAuthUserProvider userProvider) {
     this.projectCache = projectCache;
     this.lfsConfigFactory = lfsConfigFactory;
     this.repoResolver = repoResolver;
+    this.userProvider = userProvider;
   }
 
   @Override
   protected LargeFileRepository getLargeFileRepository(
-      LfsRequest request, String path) throws LfsException {
+      LfsRequest request, String path, LfsGerritProtocolServlet.AuthInfo auth)
+          throws LfsException {
     String pathInfo = path.startsWith("/") ? path : "/" + path;
     Matcher matcher = URL_PATTERN.matcher(pathInfo);
     if (!matcher.matches()) {
@@ -67,11 +76,14 @@ public class LfsApiServlet extends LfsProtocolServlet {
         ProjectUtil.stripGitSuffix(matcher.group(1)));
     ProjectState state = projectCache.get(project);
 
+    authorizeUser(userProvider.getUser(auth.auth, auth.cs),
+        state, request.getOperation());
+
     if (state == null || state.getProject().getState() == HIDDEN) {
       throw new LfsRepositoryNotFound(project.get());
     }
 
-    if (request.getOperation().equals("upload")
+    if (request.getOperation().equals(UPLOAD)
         && state.getProject().getState() == READ_ONLY) {
       throw new LfsRepositoryReadOnly(project.get());
     }
@@ -82,7 +94,7 @@ public class LfsApiServlet extends LfsProtocolServlet {
     // No config means we default to "not enabled".
     if (config != null && config.isEnabled()) {
       // For uploads, check object sizes against limit if configured
-      if (request.getOperation().equals("upload")) {
+      if (request.getOperation().equals(UPLOAD)) {
         if (config.isReadOnly()) {
           throw new LfsRepositoryReadOnly(project.get());
         }
@@ -103,5 +115,26 @@ public class LfsApiServlet extends LfsProtocolServlet {
     }
 
     throw new LfsUnavailable(project.get());
+  }
+
+  private void authorizeUser(CurrentUser user, ProjectState state,
+      String operation) throws LfsUnauthorized {
+    ProjectControl control = state.controlFor(user);
+    switch(operation) {
+      case "download":
+        if (control.isReadable()) {
+          return;
+        }
+        break;
+
+      case UPLOAD:
+        if (Capable.OK == control.canPushToAtLeastOneRef()) {
+          return;
+        }
+    }
+
+    throw new LfsUnauthorized(
+        String.format("User %s is not authorized to perform %s operation",
+            user.getUserName(), operation));
   }
 }
---
+++ src/main/java/com/googlesource/gerrit/plugins/lfs/LfsAuthUserProvider.java
@@ -0,0 +1,74 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.lfs;
+
+import com.google.common.base.Strings;
+import com.google.gerrit.server.AnonymousUser;
+import com.google.gerrit.server.CurrentUser;
+import com.google.gerrit.server.IdentifiedUser;
+import com.google.gerrit.server.account.AccountCache;
+import com.google.gerrit.server.account.AccountState;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.google.inject.Singleton;
+
+import org.apache.commons.codec.binary.Base64;
+
+import java.nio.charset.Charset;
+
+@Singleton
+public class LfsAuthUserProvider {
+  private static final String BASIC_AUTH_PREFIX = "Basic ";
+
+  private final Provider<AnonymousUser> anonymous;
+  private final AccountCache accounts;
+  private final IdentifiedUser.GenericFactory userFactory;
+
+  @Inject
+  LfsAuthUserProvider(Provider<AnonymousUser> anonymous,
+      AccountCache accounts,
+      IdentifiedUser.GenericFactory userFactory) {
+    this.anonymous = anonymous;
+    this.accounts = accounts;
+    this.userFactory = userFactory;
+  }
+
+  CurrentUser getUser(String auth, Charset cs) {
+    if (Strings.isNullOrEmpty(auth)) {
+      return anonymous.get();
+    }
+
+    if (auth.startsWith(BASIC_AUTH_PREFIX)) {
+      String usernamePassword;
+      usernamePassword = new String(
+          Base64.decodeBase64(auth.substring(BASIC_AUTH_PREFIX.length())), cs);
+
+      String username;
+      int splitPos = usernamePassword.indexOf(':');
+      if (splitPos < 0) {
+        username = usernamePassword;
+      } else {
+        username = usernamePassword.substring(0, splitPos);
+      }
+
+      AccountState acc = accounts.getByUsername(username);
+      if (acc != null) {
+        return userFactory.create(acc);
+      }
+    }
+
+    return anonymous.get();
+  }
+}
