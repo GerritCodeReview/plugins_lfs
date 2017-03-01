abb50b79f00234b374d6add1ac981c6136ab7036
David Pursehouse <dpursehouse@collab.net>
2017-03-01 19:26:43 +0900

    Add check for lfs.plugin setting on startup
    Add a check to confirm that the lfs.plugin configuration value is
    set to the expected value. If it is not, log a warning.
    
    Change-Id: I605d1ddd49597e4c2f71bbfea11f985996c2ffb6

---
+++ src/main/java/com/googlesource/gerrit/plugins/lfs/Lifecycle.java
@@ -0,0 +1,60 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.lfs;
+
+import org.eclipse.jgit.lib.Config;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Strings;
+import com.google.gerrit.extensions.annotations.PluginName;
+import com.google.gerrit.extensions.events.LifecycleListener;
+import com.google.gerrit.server.config.GerritServerConfig;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+@Singleton
+public class Lifecycle implements LifecycleListener {
+  private static final Logger log = LoggerFactory.getLogger(Lifecycle.class);
+
+  private final String name;
+  private final Config config;
+
+  @Inject
+  Lifecycle(@PluginName String name, @GerritServerConfig Config config) {
+    this.name = name;
+    this.config = config;
+  }
+
+  @Override
+  public void start() {
+    String plugin = config.getString("lfs", null, "plugin");
+    if (Strings.isNullOrEmpty(plugin)) {
+      warn("lfs.plugin is not set");
+    } else if (!plugin.equals(name)) {
+      warn(String.format("lfs.plugin is set, but is not set to '%s'", name));
+    }
+  }
+
+  private void warn(String msg) {
+    log.warn(String.format(
+        "%s; LFS will not be enabled. Run site initialization, or manually set"
+        + " lfs.plugin to '%s' in gerrit.config", msg, name));
+  }
+
+  @Override
+  public void stop() {
+  }
+}
