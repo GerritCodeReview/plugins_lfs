348a2c4e97b47271c4a447c8e758af3efce258c1
Jacek Centkowski <jcentkowski@collab.net>
2017-04-25 17:46:55 +0200

    Create REST stub for Git LFS 2.0 Lock API
    The change introduces `locks` endpoint with stubs for the following
    operations (according to [1]):
      POST /locks - create lock
      GET /locks - list locks
      POST /locks/verify - list locks for verification
      POST /locks/lock_id/unlock - delete lock
    
    [1] https://github.com/git-lfs/git-lfs/blob/master/docs/api/locking.md
    Depends-On: I8299000c827b5a34d6de1ed5fc650f74be4164a2
    Change-Id: I7e2debbc7a3b63694139f4759805863b37e1fd48
    Signed-off-by: Jacek Centkowski <jcentkowski@collab.net>

---
+++ src/test/java/com/googlesource/gerrit/plugins/lfs/LfsPathsTest.java
@@ -0,0 +1,100 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.lfs;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.junit.Test;
+
+public class LfsPathsTest {
+  // this tests the same functionality as it is being tested in
+  // com.google.gerrit.httpd.plugins.LfsPluginServletTest however in plugin
+  private static final String[] URL_PREFIXES = new String[] {"/", "/a/", "/p/", "/a/p/"};
+
+  @Test
+  public void lfsLocksRegexMatch() {
+    Pattern p = Pattern.compile(LfsPaths.LFS_LOCKS_PATH_REGEX);
+
+    matches(p, "locks/lock_id/unlock", "lock_id");
+  }
+
+  @Test
+  public void lfsLocksRegexDoesntMatch() {
+    Pattern p = Pattern.compile(LfsPaths.LFS_LOCKS_PATH_REGEX);
+
+    doesNotMatch(p, "locks/unlock");
+    doesNotMatch(p, "something/locks/lock_id/unlock");
+    doesNotMatch(p, "locks/lock_id");
+  }
+
+  @Test
+  public void lfsUrlRegexMatch() {
+    Pattern p = Pattern.compile(LfsPaths.URL_REGEX);
+    testProjectGetsMatched(p, "foo/bar/info/lfs/objects/batch", "foo/bar");
+    testProjectGetsMatched(p, "foo/bar/info/lfs/locks", "foo/bar");
+    testProjectGetsMatched(p, "foo/bar/info/lfs/locks/verify", "foo/bar");
+    testProjectAndLockIdGetMatched(
+        p, "foo/bar/info/lfs/locks/lock_id/unlock", "foo/bar", "lock_id");
+  }
+
+  @Test
+  public void lfsUrlRegexDoesntMatch() {
+    Pattern p = Pattern.compile(LfsPaths.URL_REGEX);
+    doesNotMatch(p, "/foo");
+    doesNotMatch(p, "/a/foo");
+    doesNotMatch(p, "/p/foo");
+    doesNotMatch(p, "/a/p/foo");
+
+    doesNotMatch(p, "/info/lfs/objects/batch");
+    doesNotMatch(p, "/info/lfs/objects/batch/foo");
+    doesNotMatch(p, "/info/lfs/locks");
+    doesNotMatch(p, "/info/lfs/locks/verify");
+    doesNotMatch(p, "/info/lfs/locks/unlock");
+    doesNotMatch(p, "/info/lfs/locks/lock_id/unlock");
+  }
+
+  private void testProjectAndLockIdGetMatched(
+      Pattern p, String url, String expectedProject, String expectedLockId) {
+    for (String prefix : URL_PREFIXES) {
+      matches(p, prefix + url, expectedProject, expectedLockId);
+    }
+  }
+
+  private void testProjectGetsMatched(Pattern p, String url, String expected) {
+    for (String prefix : URL_PREFIXES) {
+      matches(p, prefix + url, expected);
+    }
+  }
+
+  private void matches(Pattern p, String input, String expectedProjectName, String expectedLockId) {
+    Matcher m = p.matcher(input);
+    assertThat(m.matches()).isTrue();
+    assertThat(m.group(1)).isEqualTo(expectedProjectName);
+    assertThat(m.group(2)).isEqualTo(expectedLockId);
+  }
+
+  private void matches(Pattern p, String input, String group1) {
+    Matcher m = p.matcher(input);
+    assertThat(m.matches()).isTrue();
+    assertThat(m.group(1)).isEqualTo(group1);
+  }
+
+  private void doesNotMatch(Pattern p, String input) {
+    Matcher m = p.matcher(input);
+    assertThat(m.matches()).isFalse();
+  }
+}
