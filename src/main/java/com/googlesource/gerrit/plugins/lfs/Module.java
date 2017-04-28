519579c1c7f233981b10e75c5fb93b5957012297
Jacek Centkowski <jcentkowski@collab.net>
2017-04-28 09:34:21 +0200

    Unify exceptions handling and error reporting with LfsApiServlet
    Introduce abstract LfsLocksAction that unifies exception handling with
    LfsException (enables re-use of existing LfsExcpetion derived classes in
    the following patches) which is in line with LfsApiServlet. Send errors
    back in the way that it is understood by Git LFS client (again in line
    with LfsApiServlet).
    
    Change-Id: Id32cada65d441b148a6611a3477b4b2dd56cc5b9
    Signed-off-by: Jacek Centkowski <jcentkowski@collab.net>

--- src/main/java/com/googlesource/gerrit/plugins/lfs/Module.java
+++ src/main/java/com/googlesource/gerrit/plugins/lfs/Module.java
@@ -22,6 +22,7 @@ import com.google.gerrit.extensions.restapi.RestApiModule;
 import com.google.inject.internal.UniqueAnnotations;
 import com.googlesource.gerrit.plugins.lfs.fs.LfsFsContentServlet;
 import com.googlesource.gerrit.plugins.lfs.fs.LocalLargeFileRepository;
+import com.googlesource.gerrit.plugins.lfs.locks.LfsLocksModule;
 import com.googlesource.gerrit.plugins.lfs.s3.S3LargeFileRepository;
 
 public class Module extends FactoryModule {
@@ -43,5 +44,6 @@ public class Module extends FactoryModule {
     factory(S3LargeFileRepository.Factory.class);
     factory(LocalLargeFileRepository.Factory.class);
     factory(LfsFsContentServlet.Factory.class);
+    install(new LfsLocksModule());
   }
 }
