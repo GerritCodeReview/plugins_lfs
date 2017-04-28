05a73f7df54c02501352a610e7bdda058ac122e4
Jacek Centkowski <jcentkowski@collab.net>
2017-04-28 09:34:21 +0200

    Unify exceptions handling and error reporting with LfsApiServlet
    Introduce abstract LfsLocksAction (refactor LfsLocksServlet) so that
    exception handling is based on LfsException handling (enables re-use of
    existing LfsException derived classes in the following changes) which is
    in line with LfsApiServlet.
    Send errors back in the way that it is understood by Git LFS client
    (again in line with LfsApiServlet).
    
    Change-Id: Id32cada65d441b148a6611a3477b4b2dd56cc5b9
    Signed-off-by: Jacek Centkowski <jcentkowski@collab.net>

--- src/main/java/com/googlesource/gerrit/plugins/lfs/locks/LfsLocksServlet.java
+++ src/main/java/com/googlesource/gerrit/plugins/lfs/locks/LfsLocksServlet.java
@@ -17,266 +17,42 @@ package com.googlesource.gerrit.plugins.lfs.locks;
 import static com.googlesource.gerrit.plugins.lfs.LfsPaths.LFS_LOCKS_PATH_REGEX;
 import static com.googlesource.gerrit.plugins.lfs.LfsPaths.LFS_VERIFICATION_PATH;
 import static com.googlesource.gerrit.plugins.lfs.LfsPaths.URL_REGEX_TEMPLATE;
-import static java.nio.charset.StandardCharsets.UTF_8;
-import static org.apache.http.HttpStatus.SC_INTERNAL_SERVER_ERROR;
-import static org.apache.http.HttpStatus.SC_OK;
 
-import com.google.common.base.Strings;
-import com.google.common.base.Supplier;
-import com.google.common.base.Suppliers;
-import com.google.common.collect.ImmutableList;
-import com.google.gson.FieldNamingPolicy;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.Reader;
-import java.io.Writer;
-import java.util.Collections;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
-import org.joda.time.format.DateTimeFormatter;
-import org.joda.time.format.ISODateTimeFormat;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Singleton
 public class LfsLocksServlet extends HttpServlet {
   private static final long serialVersionUID = 1L;
-  private static final String CONTENTTYPE_VND_GIT_LFS_JSON =
-      "application/vnd.git-lfs+json; charset=utf-8";
-  private static final Pattern LFS_LOCKS_URL =
-      Pattern.compile(String.format(URL_REGEX_TEMPLATE, LFS_LOCKS_PATH_REGEX));
-  private static final Pattern LFS_VERIFICATION_URL =
-      Pattern.compile(String.format(URL_REGEX_TEMPLATE, LFS_VERIFICATION_PATH));
-  private static final Logger log = LoggerFactory.getLogger(LfsLocksServlet.class);
-  private static final DateTimeFormatter ISO = ISODateTimeFormat.dateTime();
 
   public static final String LFS_LOCKS_REST =
       String.format(URL_REGEX_TEMPLATE, LFS_LOCKS_PATH_REGEX + "|" + LFS_VERIFICATION_PATH);
 
+  private final LfsGetLocksAction.Factory getters;
+  private final LfsPutLocksAction.Factory putters;
+
+  @Inject
+  LfsLocksServlet(LfsGetLocksAction.Factory getters, LfsPutLocksAction.Factory putters) {
+    this.getters = getters;
+    this.putters = putters;
+  }
+
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
-    Action action = new Action(req, resp);
-    Matcher matcher = LFS_LOCKS_URL.matcher(action.path);
-    if (matcher.matches()) {
-      String project = matcher.group(1);
-      listLocks(project, action);
-      return;
-    }
-
-    action.sendError(
-        SC_INTERNAL_SERVER_ERROR, String.format("Unsupported path %s was provided", action.path));
+    LfsLocksContext context = new LfsLocksContext(req, resp);
+    getters.create(context).run();
   }
 
   @Override
   protected void doPost(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
-    Action action = new Action(req, resp);
-    Matcher matcher = LFS_LOCKS_URL.matcher(action.path);
-    if (matcher.matches()) {
-      String project = matcher.group(1);
-      String lockId = matcher.group(2);
-      if (Strings.isNullOrEmpty(lockId)) {
-        createLock(project, action);
-      } else {
-        deleteLock(project, lockId, action);
-      }
-      return;
-    }
-
-    matcher = LFS_VERIFICATION_URL.matcher(action.path);
-    if (matcher.matches()) {
-      verifyLocks(matcher.group(1), action);
-      return;
-    }
-
-    action.sendError(
-        SC_INTERNAL_SERVER_ERROR, String.format("Unsupported path %s was provided", action.path));
-  }
-
-  private void verifyLocks(String project, Action action) throws IOException {
-    log.debug("Verify list of locks for {} project", project);
-    //TODO method stub for verifying locks
-    action.sendResponse(
-        new LfsVerifyLocksResponse(Collections.emptyList(), Collections.emptyList(), null));
-  }
-
-  private void listLocks(String project, Action action) throws IOException {
-    log.debug("Get list of locks for {} project", project);
-    //TODO method stub for getting project's locks list
-
-    // stub for searching lock by path
-    String path = action.getParam("path");
-    if (!Strings.isNullOrEmpty(path)) {
-      action.sendResponse(
-          new LfsGetLocksResponse(
-              ImmutableList.<LfsLock>builder()
-                  .add(
-                      new LfsLock(
-                          "random_id",
-                          path,
-                          now(),
-                          new LfsLockOwner("Lock Owner <lock_owner@example.com>")))
-                  .build(),
-              null));
-      return;
-    }
-
-    // stub for searching lock by id
-    String id = action.getParam("id");
-    if (!Strings.isNullOrEmpty(id)) {
-      action.sendResponse(
-          new LfsGetLocksResponse(
-              ImmutableList.<LfsLock>builder()
-                  .add(
-                      new LfsLock(
-                          id,
-                          "path/to/file",
-                          now(),
-                          new LfsLockOwner("Lock Owner <lock_owner@example.com>")))
-                  .build(),
-              null));
-      return;
-    }
-
-    // stub for returning all locks
-    action.sendResponse(new LfsGetLocksResponse(Collections.emptyList(), null));
-  }
-
-  private void deleteLock(String project, String lockId, Action action) throws IOException {
-    LfsDeleteLockInput input = action.input(LfsDeleteLockInput.class);
-    log.debug(
-        "Delete (-f {}) lock for {} in project {}",
-        Boolean.TRUE.equals(input.force),
-        lockId,
-        project);
-    //TODO: this is just the method stub for lock deletion
-    LfsLock lock =
-        new LfsLock(
-            "random_id",
-            "some/path/to/file",
-            now(),
-            new LfsLockOwner("Lock Owner <lock_owner@example.com>"));
-    action.sendResponse(lock);
-  }
-
-  private void createLock(String project, Action action) throws IOException {
-    LfsCreateLockInput input = action.input(LfsCreateLockInput.class);
-    log.debug("Create lock for {} in project {}", input.path, project);
-    //TODO: this is just the method stub lock creation
-    LfsLock lock =
-        new LfsLock(
-            "random_id",
-            input.path,
-            now(),
-            new LfsLockOwner("Lock Owner <lock_owner@example.com>"));
-    action.sendResponse(lock);
-  }
-
-  private String now() {
-    return ISO.print(DateTime.now().toDateTime(DateTimeZone.UTC));
-  }
-
-  private class Action {
-    public final String path;
-
-    private final HttpServletRequest req;
-    private final HttpServletResponse res;
-    private final Supplier<Writer> writer;
-    private final Supplier<Reader> reader;
-    private final Gson gson;
-
-    private Action(final HttpServletRequest req, final HttpServletResponse res) {
-      this.path = req.getPathInfo().startsWith("/") ? req.getPathInfo() : "/" + req.getPathInfo();
-      this.req = req;
-      this.res = res;
-      this.writer =
-          Suppliers.memoize(
-              new Supplier<Writer>() {
-                @Override
-                public Writer get() {
-                  try {
-                    return new BufferedWriter(new OutputStreamWriter(res.getOutputStream(), UTF_8));
-                  } catch (IOException e) {
-                    throw new RuntimeException(e);
-                  }
-                }
-              });
-      this.reader =
-          Suppliers.memoize(
-              new Supplier<Reader>() {
-                @Override
-                public Reader get() {
-                  try {
-                    return new BufferedReader(new InputStreamReader(req.getInputStream(), UTF_8));
-                  } catch (IOException e) {
-                    throw new RuntimeException(e);
-                  }
-                }
-              });
-      this.gson = createGson();
-      setLfsResponseType();
-    }
-
-    String getParam(String name) {
-      return req.getParameter(name);
-    }
-
-    <T> T input(Class<T> clazz) {
-      return gson.fromJson(getReader(), clazz);
-    }
-
-    <T> void sendResponse(T content) throws IOException {
-      res.setStatus(SC_OK);
-      gson.toJson(content, getWriter());
-      getWriter().flush();
-    }
-
-    void sendError(int status, String message) throws IOException {
-      log.error(message);
-      res.setStatus(status);
-      gson.toJson(new Error(message), getWriter());
-      getWriter().flush();
-    }
-
-    Writer getWriter() {
-      return writer.get();
-    }
-
-    Reader getReader() {
-      return reader.get();
-    }
-
-    void setLfsResponseType() {
-      res.setContentType(CONTENTTYPE_VND_GIT_LFS_JSON);
-    }
-
-    private Gson createGson() {
-      return new GsonBuilder()
-          .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
-          .disableHtmlEscaping()
-          .create();
-    }
-  }
-
-  /** copied from org.eclipse.jgit.lfs.server.LfsProtocolServlet.Error */
-  static class Error {
-    String message;
-
-    Error(String m) {
-      this.message = m;
-    }
+    LfsLocksContext context = new LfsLocksContext(req, resp);
+    putters.create(context).run();
   }
 }
