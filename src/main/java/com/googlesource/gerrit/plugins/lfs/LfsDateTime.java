11f793bce46ac3e56c53a0104467ed583f05f474
David Pursehouse <dpursehouse@collab.net>
2017-11-02 16:35:51 +0900

    Replace joda-time with Java 8 API
    Since Iccfdb1e20 joda-time is no longer exported in the plugin API.
    
    joda-time was used for formatting dates to strings. Factor this
    formatting out to a utility class.
    
    Change-Id: I49bffffbd171065c6b3c0803a5a2bb377bd27ef1

---
+++ src/main/java/com/googlesource/gerrit/plugins/lfs/LfsDateTime.java
@@ -0,0 +1,36 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.lfs;
+
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+public class LfsDateTime {
+  private static final DateTimeFormatter FORMAT =
+      DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSX").withZone(ZoneOffset.UTC);
+
+  public static String now() {
+    return FORMAT.format(Instant.now());
+  }
+
+  public static String now(int secondsToAdd) {
+    return FORMAT.format(Instant.now().plusSeconds(secondsToAdd));
+  }
+
+  public static String format(Instant instant) {
+    return FORMAT.format(instant);
+  }
+}
