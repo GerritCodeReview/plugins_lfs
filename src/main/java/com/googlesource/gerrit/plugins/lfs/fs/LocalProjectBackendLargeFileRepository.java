f1e3d7dfe34d5013f6e948efa6cb575b124e415e
Jacek Centkowski <jcentkowski@collab.net>
2018-11-14 17:31:48 -0800

    RFC: Introduce repository based FS backend
    Despite consuming potentially more space it makes maintenance
    (move, delete, etc) way easier.
    
    Current FS based LFS repository structure:
      [backend_dir]
        └── XX
          └── YY
            └── XXYY...// long object id
    
    Proposed structure:
      [backend_dir]
        └── repositories
          └── [repositoryN]
            └── XX
              └── YY
                └── XXYY...// long object id
    
    intermediate 'repositories' dir was proposed so that it is easier
    to remove objects when migtation to new structure is performed and
    there is no chance that repository name collides with 'lfs_locks'
    directory which is reserved for locks.
    
    I imagine follow up changes that:
    1. introduce migratorator from FS to FS_WITH_REPOS backend type.
    2. cache LocalProjectLargeFileRepository instances in
    LfsRepositoryResolver when API request is issued so that we have
    extra validation in LfsFsRepoContentServlet when content is handled
    (if repo is not in cache it means that someone tampered request).
    3. consider sharing more code between FS and FS_WITH_REPOS backend
    implementation
    
    Change-Id: I3c9a32e3ac0f4ac3526353f48d76e591e14069b3

---
+++ src/main/java/com/googlesource/gerrit/plugins/lfs/fs/LocalProjectBackendLargeFileRepository.java
@@ -0,0 +1,103 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.lfs.fs;
+
+import static com.googlesource.gerrit.plugins.lfs.fs.LocalLargeFileRepository.getContentPath;
+import static com.googlesource.gerrit.plugins.lfs.fs.LocalLargeFileRepository.getOrCreateDataDir;
+
+import com.google.gerrit.extensions.annotations.PluginCanonicalWebUrl;
+import com.google.gerrit.extensions.annotations.PluginData;
+import com.google.inject.Inject;
+import com.google.inject.assistedinject.Assisted;
+import com.googlesource.gerrit.plugins.lfs.LfsBackend;
+import com.googlesource.gerrit.plugins.lfs.LfsConfigurationFactory;
+import com.googlesource.gerrit.plugins.lfs.LfsGlobalConfig;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.eclipse.jgit.lfs.lib.AnyLongObjectId;
+import org.eclipse.jgit.lfs.server.Response;
+import org.eclipse.jgit.lfs.server.fs.FileLfsRepository;
+
+public class LocalProjectBackendLargeFileRepository extends FileLfsRepository {
+  public interface Factory {
+    LocalProjectBackendLargeFileRepository create(LfsBackend backend);
+  }
+
+  static final String ROOT_DIR = "repository";
+
+  private final LocalProjectLargeFileRepository.Factory repoLfs;
+  private final LfsBackend backend;
+  private final String servletUrlPattern;
+
+  @Inject
+  LocalProjectBackendLargeFileRepository(
+      LocalProjectLargeFileRepository.Factory repoLfs,
+      LfsConfigurationFactory configFactory,
+      @PluginCanonicalWebUrl String url,
+      @PluginData Path defaultDataDir,
+      @Assisted LfsBackend backend)
+      throws IOException {
+    super(null, getOrCreateRootDataDir(configFactory.getGlobalConfig(), backend, defaultDataDir));
+    this.repoLfs = repoLfs;
+    this.backend = backend;
+    this.servletUrlPattern = "/" + getContentPath(backend) + "*";
+  }
+
+  public String getServletUrlPattern() {
+    return servletUrlPattern;
+  }
+
+  public FileLfsRepository getRepository(String repo) {
+    return repoLfs.create(backend, repo);
+  }
+
+  @Override
+  public Response.Action getDownloadAction(AnyLongObjectId id) {
+    throw new UnsupportedOperationException(
+        "This is LFS FS repository proxy shouldn't be used to get data");
+  }
+
+  @Override
+  public Response.Action getUploadAction(AnyLongObjectId id, long size) {
+    throw new UnsupportedOperationException(
+        "This is LFS FS repository proxy shouldn't be used to upload data");
+  }
+
+  @Override
+  public Response.Action getVerifyAction(AnyLongObjectId id) {
+    throw new UnsupportedOperationException(
+        "This is LFS FS repository proxy shouldn't be used to verify data");
+  }
+
+  @Override
+  public long getSize(AnyLongObjectId id) throws IOException {
+    throw new UnsupportedOperationException(
+        "This is LFS FS repository proxy shouldn't be used to get data size");
+  }
+
+  private static Path getOrCreateRootDataDir(
+      LfsGlobalConfig cfg, LfsBackend backend, Path defaultDataDir)
+      throws IOException {
+    Path backendPath = getOrCreateDataDir(cfg, backend, defaultDataDir);
+    Path ensured = Files.createDirectories(backendPath.resolve(ROOT_DIR));
+
+    if (!Files.isReadable(ensured)) {
+      throw new IOException("Path '" + ensured.toAbsolutePath() + "' cannot be accessed");
+    }
+
+    return ensured;
+  }
+}
